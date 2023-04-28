6470693563ffb36811db3fd9cc91845fb2f00b02
Asmolovsky <asmolovsky89@gmail.com>
2023-04-28 09:29:45 +0200

    TypeError

--- les1.py
+++ les1.py
@@ -1,7 +1,4 @@
-a = int(input('Введите число 1 '))
-b = int(input('Введите число 2 '))
-c = int(input('Введите число 3 '))
-if a+b>c and a+c>b and b+c>a:
-    print('Существует')
-else:
-    print('Не существует')
+try:
+    c = 1+"два"
+except TypeError:
+    print("Неверный тип переменной!")
