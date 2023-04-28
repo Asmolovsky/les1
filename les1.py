64498ee65b91468310a797528dedc3b950b98c74
Asmolovsky <asmolovsky89@gmail.com>
2023-04-28 14:35:42 +0200

    YearsOld

--- les1.py
+++ les1.py
@@ -1,5 +1,21 @@
-try:
-    a = int(input("Введите число: "))
-    print("Введенное число: ", a)
-except ValueError:
-    print("Некорректное значение числа")
+TodayYear = int(input("Введите год сегодня: "))
+TodayMonth = int(input("Введите месяц сегодня: "))
+TodayDay = int(input("Введите день сегодня: "))
+BdayYear = int(input("Введите год рождения: "))
+BdayMonth = int(input("Введите месяц рождения: "))
+BdayDay = int(input("Введите день рождения: "))
+YearOld = TodayYear - BdayYear
+MonthAge = 12-BdayMonth+TodayMonth
+DayAge = 30-BdayDay+TodayDay
+if MonthAge>12 or (MonthAge+1)>12:
+    print(YearOld+1)
+    if DayAge>=30 and (MonthAge+1)>=12:
+        print("На 1 год больше")
+else:
+    print(YearOld)
+
+
+
+
+
+
