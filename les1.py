0f5f32b92077cd53e2e577a704a32b8218fac3ea
Asmolovsky <asmolovsky89@gmail.com>
2023-05-02 13:26:01 +0200

    3sign number

--- les1.py
+++ les1.py
@@ -1,18 +1,17 @@
-TodayYear = int(input("Введите год сегодня: "))
-TodayMonth = int(input("Введите месяц сегодня: "))
-TodayDay = int(input("Введите день сегодня: "))
-BdayYear = int(input("Введите год рождения: "))
-BdayMonth = int(input("Введите месяц рождения: "))
-BdayDay = int(input("Введите день рождения: "))
-YearOld = TodayYear - BdayYear
-MonthAge = 12-BdayMonth+TodayMonth
-DayAge = 30-BdayDay+TodayDay
-if MonthAge>12 or (MonthAge+1)>12:
-    print(YearOld+1)
-    if DayAge>=30 and (MonthAge+1)>=12:
-        print("На 1 год больше")
-else:
-    print(YearOld)
+try:
+    number=(input("Введите трехзначное число: "))
+    a=int(number)//100
+    c=int(number)%10
+    b=int(number)//10%10
+    print(a+b+c)
+except ValueError:
+    print("Пожалуйста вводите цифры")
+
+
+
+
+
+
 
 
 
