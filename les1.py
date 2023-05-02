16d634398f2cfdae2a6e2d50bf797d8c64b87cc7
Asmolovsky <asmolovsky89@gmail.com>
2023-05-02 16:38:50 +0200

    CompareOfAge

--- les1.py
+++ les1.py
@@ -1,11 +1,26 @@
 try:
-    number=(input("Введите трехзначное число: "))
-    a=int(number)//100
-    c=int(number)%10
-    b=int(number)//10%10
-    print(a+b+c)
+    Year1 = int(input("Введите год1: "))
+    Month1 = int(input("Введите месяц1: "))
+    Day1 = int(input("Введите день1: "))
+    Year2 = int(input("Введите год2: "))
+    Month2 = int(input("Введите месяц2: "))
+    Day2 = int(input("Введите день2: "))
+    if Year1>Year2:
+        print("Второй человек старше")
+    elif Year2>Year1:
+        print("Первый человек старше")
+    elif Year1==Year2:
+        if Month1>Month2:
+            print("Второй человек старше")
+        elif Month2>Month1:
+            print("Первый человек старше")
+        elif Month1==Month2:
+            if Day1>Day2:
+                print("Второй человек старше")
+            elif Day2>Day1:
+                print("Первый человек старше")
 except ValueError:
-    print("Пожалуйста вводите цифры")
+    print("Вы ввели не число!")
 
 
 
