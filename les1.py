2db73afc907d3fda03ef98aa04f7c5d864b747ee
Asmolovsky <asmolovsky89@gmail.com>
2023-05-15 14:57:25 +0200

    Sum and multiplication

--- les1.py
+++ les1.py
@@ -1,26 +1,9 @@
-try:
-    Year1 = int(input("Введите год1: "))
-    Month1 = int(input("Введите месяц1: "))
-    Day1 = int(input("Введите день1: "))
-    Year2 = int(input("Введите год2: "))
-    Month2 = int(input("Введите месяц2: "))
-    Day2 = int(input("Введите день2: "))
-    if Year1>Year2:
-        print("Второй человек старше")
-    elif Year2>Year1:
-        print("Первый человек старше")
-    elif Year1==Year2:
-        if Month1>Month2:
-            print("Второй человек старше")
-        elif Month2>Month1:
-            print("Первый человек старше")
-        elif Month1==Month2:
-            if Day1>Day2:
-                print("Второй человек старше")
-            elif Day2>Day1:
-                print("Первый человек старше")
-except ValueError:
-    print("Вы ввели не число!")
+n = input("Введите трехзначное натуральное число: ")
+a = int(n[0])
+b = int(n[1])
+c = int(n[2])
+print("Сумма цифр числа:", a + b + c)
+print("Произведение цифр числа: ", a*b*c)
 
 
 
