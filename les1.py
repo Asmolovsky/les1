af1412404cc595f2cf3fcc27b7745ee3e0e40108
Asmolovsky <asmolovsky89@gmail.com>
2023-08-02 16:16:23 +0200

    while

--- les1.py
+++ les1.py
@@ -1,8 +1,11 @@
-a = int(input("Введите минимум: "))
-b = int(input("Введите максимум: "))
-c = int(input("Введите шаг: "))
-for i in range(a,b,c):
-     print(i)
+a = int(input('Введите число '))
+m = a%10
+a = a//10
+while a > 0:
+    if a%10 > m:
+        m = a%10
+    a = a//10
+print(m)
 
 
 
