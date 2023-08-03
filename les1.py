f3dead755f23dc27849f19e82740c5b0255e7d21
Asmolovsky <asmolovsky89@gmail.com>
2023-08-03 20:56:31 +0200

    fibandfact

--- les1.py
+++ les1.py
@@ -1,16 +1,20 @@
-a = int(input('Введите число '))
-m = a%10
-a = a//10
-while a > 0:
-    if a%10 > m:
-        m = a%10
-    a = a//10
-print(m)
-
-a = input('Введите целое число: ')
-b = a[::-1]
-print('Обратное ему число:', b)
-
+n = int(input('Введите число: '))
+f1 = 1
+f2 = 2
+print(f1, f2, end=" ")
+for i in range(3,n+1):
+    print(f1+f2, end=" ")
+    b = f1
+    f1 = f2
+    f2 = b+f1
+print ()
+print ()
+n = int(input('Введите число: '))
+factorial = 1
+while n > 1:
+    factorial *= n
+    n -= 1
+print(factorial)
 
 
 
