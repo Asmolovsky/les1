1d816b25350c9ad990d22dd5cdc374995ccff449
Asmolovsky <asmolovsky89@gmail.com>
2023-09-14 19:19:18 +0200

    cycle11

--- les1.py
+++ les1.py
@@ -1,20 +1,6 @@
-n = int(input('Введите число: '))
-f1 = 1
-f2 = 2
-print(f1, f2, end=" ")
-for i in range(3,n+1):
-    print(f1+f2, end=" ")
-    b = f1
-    f1 = f2
-    f2 = b+f1
-print ()
-print ()
-n = int(input('Введите число: '))
-factorial = 1
-while n > 1:
-    factorial *= n
-    n -= 1
-print(factorial)
+for i in range(10000+1):
+    if i%11==0:
+        print(str(i)+ " делится на 11")
 
 
 
